10ce869ec630d1ef2b380c707ef97b28b1e3233a
luisalvesmartinsms <luis.martins@microsoft.com>
2019-03-04 19:04:25 +0000

    adding pragmas

--- bot.js
+++ bot.js
@@ -6,6 +6,7 @@ const { TurnContext } = require('botbuilder-core');
 const requestpromise = require('request-promise');
 
 var conversationList=[];
+var childBotList=[];
 
 class MyBot {
     /**
@@ -15,46 +16,63 @@ class MyBot {
     async onTurn(turnContext) {
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         if (turnContext.activity.type === ActivityTypes.Message) {
+            var message=turnContext.activity.text;
+            if (message.startsWith('#')){
+                if (message.toUpperCase().startsWith('#ADD{')){
+                    var json=message.substring(4);
+                    childBotList.push(JSON.parse(json));
+                    turnContext.sendActivity("ChildBot " + childBotList[childBotList.length-1].id + " added");
+                    return;
+                } 
+                if (message.toUpperCase()=='#CLEAR'){
+                    childBotList=[];
+                    turnContext.sendActivity("ChildBot list cleared");
+                    return;
+                }
+            }
 
             //#region BOT2BOT CODE
             // In a production scenario the childbot could be choosen based on an intent or a menu option
             var childBot =this.getChildBotList()[0];
+            if (childBot){
 
-            var newConversation = 
-            {
-                BotId: childBot.id,
-                ConversationId: turnContext.activity.conversation.id,
-                OriginalServiceUri: turnContext.activity.serviceUrl,
-                ConvReference: TurnContext.getConversationReference(turnContext.activity)
-            };
-            var p=conversationList.find(x=> x.BotId==childBot.id && x.OriginalServiceUri==turnContext.activity.serviceUrl);
-            if (p==null)
-            {
-                conversationList.push(newConversation);
+                var newConversation = 
+                {
+                    BotId: childBot.id,
+                    ConversationId: turnContext.activity.conversation.id,
+                    OriginalServiceUri: turnContext.activity.serviceUrl,
+                    ConvReference: TurnContext.getConversationReference(turnContext.activity)
+                };
+                var p=conversationList.find(x=> x.BotId==childBot.id && x.OriginalServiceUri==turnContext.activity.serviceUrl && x.ConversationId==turnContext.activit.conversation.id);
+                if (p==null)
+                {
+                    conversationList.push(newConversation);
+                }
+
+                // send the activity to the child bot
+                await ConversationForwardActivity(turnContext, childBot, 'http://localhost:3981');
             }
-
-            // send the activity to the child bot
-            await ConversationForwardActivity(turnContext, childBot, 'http://localhost:3981');
+            else{
+                await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
+            }
             // it will not continue to the echo bot
             //#endregion BOT2BOT CODE
-
-            await turnContext.sendActivity(`You said '${ turnContext.activity.text }'`);
         } else {
-            await turnContext.sendActivity(`[${ turnContext.activity.type } event detected]`);
+            if (turnContext.activity.membersAdded.length !== 0) {
+                for (let idx in turnContext.activity.membersAdded) {
+                    if (turnContext.activity.membersAdded[idx].id !== turnContext.activity.recipient.id) {
+                        turnContext.sendActivity("Possible commands:\n\n #ADD{\"id\":\"name\",\"appId\":\"\", \"appPassword\": \"\",\"uri\":\"http://address/api/messages\"} \n\n #CLEAR");
+                    }  
+                }
+            }
+      
         }
     }
 
     //#region BOT2BOT CODE
     // In a production scenario the childbot list could be read from storage or configuration
     getChildBotList(){
-        return [
-            {
-                id: 'one bot',
-                appId: "c7467981-4885-45bc-xxxxxxx",
-                appPassword: "nheblZUGDMN7xxxxx",
-                uri: "http://localhost:3979/api/messages",
-            }
-        ];
+        return childBotList;
     }
     
     getConversation (conversationId){
